e7015f923ab671b139a83a4d396ad3bd4c1e7006
Bettykirii <warwarekirii@gmail.com>
2021-02-03 10:09:24 +0300

    created the comment section for posts

---
+++ src/main/java/com/example/springredditclone/controller/VoteController.java
@@ -0,0 +1,4 @@
+package com.example.springredditclone.controller;
+
+public class VoteController {
+}
--- src/main/java/com/example/springredditclone/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/springredditclone/security/JwtAuthenticationFilter.java
@@ -1,13 +1,17 @@
 package com.example.springredditclone.security;
 
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,7 +21,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component
+
+@Configuration
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
@@ -25,6 +30,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 
     @Autowired
+
     private UserDetailsService userDetailsService;
 
     @Override
